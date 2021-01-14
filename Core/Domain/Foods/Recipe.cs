5429f15316d24ba9c73d1e336e380daebf7e0af5
Daylan Vig <dvig1@athabasca.edu>
2021-01-14 15:24:33 -0700

    [Feat] Add url to recipe
    - Add to entity, add migration.

--- Core/Domain/Foods/Recipe.cs
+++ Core/Domain/Foods/Recipe.cs
@@ -9,16 +9,18 @@ namespace FoodApp.Core.Domain.Foods
     {
         public string Name { get; private set; }
         public IReadOnlyList<RecipeIngredient> RecipeIngredients { get; private set; }
+        public string Url { get; private set; }
 
         public Recipe()
         {
             RecipeIngredients = new List<RecipeIngredient>().ToImmutableList();
         }
 
-        private Recipe(string name, IEnumerable<RecipeIngredient> recipeIngredients) : this()
+        private Recipe(string name, IEnumerable<RecipeIngredient> recipeIngredients, string url) : this()
         {
             Name = name;
             RecipeIngredients = recipeIngredients.ToImmutableList();
+            Url = url;
         }
 
         /// <summary>
@@ -26,10 +28,11 @@ namespace FoodApp.Core.Domain.Foods
         /// </summary>
         /// <param name="name">The name.</param>
         /// <param name="recipeIngredients">The recipe ingredients.</param>
+        /// <param name="url">Optional - URL where recipe can be found</param>
         /// <returns></returns>
-        public static Recipe CreateNew(string name, IEnumerable<RecipeIngredient> recipeIngredients)
+        public static Recipe CreateNew(string name, IEnumerable<RecipeIngredient> recipeIngredients, string url)
         {
-            return new Recipe(name, recipeIngredients);
+            return new Recipe(name, recipeIngredients, url);
         }
         
     }
---
+++ FoodApp.Data/Migrations/Data/20210114222340_AddRecipeUrl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodApp.Data.Migrations.Data
+{
+    public partial class AddRecipeUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Recipes",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Recipes");
+        }
+    }
+}
