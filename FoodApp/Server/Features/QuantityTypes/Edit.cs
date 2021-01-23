393aa3f60a5ddaff918bbc4a21a343d37fb55df2
Daylan Vig <dvig1@athabasca.edu>
2021-01-23 08:29:59 -0700

    [Feat] Add edit quantity type handler

---
+++ FoodApp/Server/Features/QuantityTypes/Edit.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FoodApp.Core.Common;
+using FoodApp.Core.Domain.QuantityTypes;
+using FoodApp.Core.Interfaces;
+using FoodApp.Shared.Models.Foods;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FoodApp.Server.Features.QuantityTypes
+{
+    public class Edit
+    {
+        public record Command(int Id, string Type) : IRequest<QuantityTypeModel>;
+
+        public class Handler : IRequestHandler<Command, QuantityTypeModel>
+        {
+            private readonly IRepository<QuantityType> _quantityTypeRepository;
+            private readonly IMapper _mapper;
+
+            public Handler(IRepository<QuantityType> quantityTypeRepository, IMapper mapper)
+            {
+                _quantityTypeRepository = quantityTypeRepository;
+                _mapper = mapper;
+            }
+            public async Task<QuantityTypeModel> Handle(Command request, CancellationToken cancellationToken = default)
+            {
+                // Here we load quantity types that have the same name for duplicate checks, or same id for updating (reduce a query)
+                var quantityTypes = await _quantityTypeRepository.ToListAsync(f =>
+                                                        f.Type.ToLower() == request.Type.ToLower() ||
+                                                        f.Id == request.Id);
+
+                var quantityType = quantityTypes.SingleOrDefault(q => q.Id == request.Id);
+                Guard.AgainstNull(quantityType, nameof(QuantityType.Id), "QuantityType does not exist.");
+
+                // Has same name but isn't the one we're editing
+                var duplicateQuantityType = quantityTypes.SingleOrDefault(f => f.Id != request.Id);
+                Guard.ShouldBeNull(duplicateQuantityType, nameof(Command.Type), $"QuantityType name is in use (ID = {duplicateQuantityType?.Id})");
+                quantityType!.UpdateType(request.Type);
+                await _quantityTypeRepository.EditAsync(quantityType);
+                return _mapper.Map<QuantityType, QuantityTypeModel>(quantityType);
+            }
+        }
+    }
+}
---
+++ Tests.FoodApp/Features/QuantityTypes/EditQuantityTypeHandler_Tests.cs
@@ -0,0 +1,83 @@
+﻿using FoodApp.Core.Domain.QuantityTypes;
+using FoodApp.Core.Interfaces;
+using FoodApp.Server.Features.QuantityTypes;
+using FoodApp.Services.Foods;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Tests.FoodApp.TestInfrastructure;
+using Xunit;
+
+namespace Tests.FoodApp.Features.QuantityTypes
+{
+    public class EditQuantityTypeHandler_Tests : DefaultTestFixture<Edit.Handler>
+    {
+        private readonly Mock<IRepository<QuantityType>> _mockQuantityTypeRepository;
+        private readonly Edit.Command _editCommand = new(2, "NAME");
+        public EditQuantityTypeHandler_Tests()
+        {
+            _mockQuantityTypeRepository = DoMockRepository<QuantityType>(null);
+        }
+
+        [Fact]
+        public async Task ThrowAnArgumentExceptionWhenIdDoesNotExist()
+        {
+            // Arrange
+            _mockQuantityTypeRepository.Setup(m => m.ToListAsync(It.IsAny<Expression<Func<QuantityType, bool>>>()))
+                .ReturnsAsync(new List<QuantityType>
+                {
+                    CreateEntity.CreateExistingQuantityType("Name", 3)
+                });
+
+            // Act + Assert
+            await Assert.ThrowsAsync<ArgumentException>("Id", () => Sut.Handle(_editCommand));
+        }
+
+        [Fact]
+        public async Task ThrowAnArgumentExceptionWhenNameIsInUseByADifferentQuantityType()
+        {
+            // Arrange
+            _mockQuantityTypeRepository.Setup(m => m.ToListAsync(It.IsAny<Expression<Func<QuantityType, bool>>>()))
+                .ReturnsAsync(new List<QuantityType>
+                {
+                    CreateEntity.CreateExistingQuantityType(_editCommand.Type, _editCommand.Id - 1),
+                    CreateEntity.CreateExistingQuantityType("AName", _editCommand.Id),
+                });
+
+            // Act + Assert
+            await Assert.ThrowsAsync<ArgumentException>("Type", () => Sut.Handle(_editCommand));
+        }
+
+        [Fact]
+        public async Task NotThrowArgumentExceptionWhenNameIsInUseByCurrentQuantityType()
+        {
+            // Arrange
+            _mockQuantityTypeRepository.Setup(m => m.ToListAsync(It.IsAny<Expression<Func<QuantityType, bool>>>()))
+                .ReturnsAsync(new List<QuantityType>
+                {
+                    CreateEntity.CreateExistingQuantityType(_editCommand.Type, _editCommand.Id)
+                });
+
+            // Act (no assert for non-throwing)
+            await Sut.Handle(_editCommand);
+        }
+
+        [Fact]
+        public async Task SetTypeNameToBeNewTypeName()
+        {
+            // Arrange
+            _mockQuantityTypeRepository.Setup(m => m.ToListAsync(It.IsAny<Expression<Func<QuantityType, bool>>>()))
+              .ReturnsAsync(new List<QuantityType>
+              {
+                    CreateEntity.CreateExistingQuantityType("OriginalName", _editCommand.Id)
+              });
+
+            // Act
+            await Sut.Handle(_editCommand);
+            // Assert
+            _mockQuantityTypeRepository.Verify(m => m.EditAsync(It.Is<QuantityType>(q => q.Type == _editCommand.Type)));
+        }
+    }
+}
